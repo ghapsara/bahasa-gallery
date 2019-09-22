7244b54061ea4f87d4b44d9b3713fe6fa77d658a
Guruh Hapsara <guruhhapsara@gmail.com>
2019-09-22 23:57:00 +0700

    fix scroll

--- src/Detail.js
+++ src/Detail.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useRef, useMemo } from 'react';
+import React, { useState, useCallback, useRef, useMemo, useEffect } from 'react';
 import styled from 'styled-components';
 import { useSpring, interpolate, config, animated } from 'react-spring';
 import { lerp, mapRange, degToRad, radToDeg, linspace } from 'canvas-sketch-util/math';
@@ -23,39 +23,36 @@ const CENTER_Y = HEIGHT * 0.5;
 const COLOR = '#ff8000';
 
 const TEXT_COLOR = '#003732';
+
 const BACKGROUND_COLOR = '#df3a46';
+
+const FONT_SIZE = 80;
+
 
 const Container = styled.div`
   position: fixed;
   top: 0;
-  height: 100vh;
-  width: 100vw;
+  left: 0;
+  height: 100%;
+  width: 100%;
   overflow: scroll;
 `;
 
 const ScrollWrapper = styled.div`
-  width: 100%;
+  position: relative;
+  width: 100vw;
   height: ${props => props.height}px;
 `;
 
 const SvgContainer = styled.div`
   position: sticky;
   top: 0;
-  margin: ;
   height: 100vh;
   width: 100vw;
-`;
-
-const Svg = styled.svg`
-  width: ${WIDTH}px;
-  height: ${HEIGHT}px;
-  view-box: 0 0 ${WIDTH} ${HEIGHT};
+  display: flex;
 `;
 
 function mapsPath(map, name) {
-  // const features = mesh(map, map.objects[name]);
-
-
   return map.objects[name].geometries.map(d => {
     const p = mesh(map, d);
 
@@ -67,31 +64,21 @@ function mapsPath(map, name) {
 };
 
 function TextMask({ textList, scrollHeight, y, ...props }) {
-  const fontSize = 160;
-  const scrollBottom = textList.length * fontSize;
+  const left = WIDTH * 0.1;
 
   return (
-    <>
-      {textList.map((text, i) => {
-        return (
-          <animated.text
-            {...props}
-            key={i}
-            transform={y
-              .interpolate(t => {
-                const tY = ((i + 1) * fontSize) - t;
-                return `translate(${CENTER_X}, ${tY})`;
-              })
-            }
-            textAnchor="middle"
-            fontSize={fontSize}
-          >
-            {text}
-          </animated.text>
-        )
-      })}
-    </>
-  )
+    <animated.text
+      {...props}
+      fontSize={FONT_SIZE}
+      alignmentBaseline="middle"
+      dominantBaseline="middle"
+      y={y.interpolate(t => -t)}
+    >
+      {textList.map(d =>
+        <tspan key={d} x={left} dy="1.2em">{d}</tspan>
+      )}
+    </animated.text>
+  );
 }
 
 function Detail() {
@@ -105,45 +92,39 @@ function Detail() {
   }, [set]);
 
 
-  const items = bahasaData['Nusa Tenggara Timur'].map(d => d.replace('Bahasa', '').trim())
+  const items = bahasaData['Kalimantan Timur'].map(d => d.replace('Bahasa', '').trim());
 
-  const itemsSpace = linspace(items.length);
-
-  const distance = 200;
-
-  const fontSize = 160;
-  const scrollBottom = (items.length + 1) * fontSize;
-
-  const scrollHeight = scrollBottom;
-
+  const scrollHeight = (items.length * (FONT_SIZE * 1.2)) + (FONT_SIZE * 1.2);
   // infinite scroll
   // scroll direction
 
   const maps = mapsPath(bali, 'bali');
 
-  const textTransform = `translate(${WIDTH * 0.5}, ${HEIGHT * 0.5})`;
   const mapsTransform = `translate(${0.1 * WIDTH}, ${0.1 * HEIGHT})`;
 
   return (
     <Container onScroll={onScroll}>
       <ScrollWrapper height={scrollHeight}>
         <SvgContainer>
-          <Svg>
+          <svg
+            width={WIDTH}
+            height={HEIGHT}
+          >
+            <rect x={0} y={0} width={WIDTH} height={HEIGHT} fill={BACKGROUND_COLOR} />
             <mask id="mask-text-inside-maps">
-              <rect x={0} y={0} width="100%" height="100%" fill="white" />
+              <rect x={0} y={0} width={WIDTH} height={HEIGHT} fill="white" />
               <TextMask scrollHeight={scrollHeight} y={scrollTop} textList={items} />
             </mask>
             <mask id="mask-text-outside-maps">
-              <rect x={0} y={0} width="100%" height="100%" fill="white" />
+              <rect x={0} y={0} width={WIDTH} height={HEIGHT} fill="white" />
               <TextMask scrollHeight={scrollHeight} y={scrollTop} textList={items} />
             </mask>
             <mask id="mask-maps">
-              <rect x={0} y={0} width="100%" height="100%" fill="white" />
+              <rect x={0} y={0} width={WIDTH} height={HEIGHT} fill="white" />
               <path d={maps[1]} transform={mapsTransform} />
             </mask>
 
-            <rect x="0" y="0" width="100%" height="100%" fill={BACKGROUND_COLOR} />
-            <rect x="0" y="0" width="100%" height="100%" fill={BACKGROUND_COLOR} mask="url(#mask-text-outside-maps)" />
+            <rect x={0} y={0} width={WIDTH} height={HEIGHT} fill={BACKGROUND_COLOR} mask="url(#mask-text-outside-maps)" />
 
             <g mask="url(#mask-maps)">
               <TextMask scrollHeight={scrollHeight} y={scrollTop} textList={items} fill={TEXT_COLOR} />
@@ -152,7 +133,13 @@ function Detail() {
             <g mask="url(#mask-text-inside-maps)">
               <path d={maps[1]} fill={TEXT_COLOR} transform={mapsTransform} />
             </g>
-          </Svg>
+            {/* <rect
+              x={0}
+              y={CENTER_Y - (FONT_SIZE * 0.5)}
+              width={WIDTH}
+              height={FONT_SIZE}
+            /> */}
+          </svg>
         </SvgContainer>
       </ScrollWrapper>
     </Container>

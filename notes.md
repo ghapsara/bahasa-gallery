88e63b472fd16713c6ae81407dffe6165e25f38f
Guruh Hapsara <guruhhapsara@gmail.com>
2019-12-26 02:14:06 +0700

    🎸

--- notes.md
+++ notes.md
@@ -13,9 +13,6 @@ map with generative art in the back
 
 https://bashooka.com/wp-content/uploads/2014/09/swiss-style-designs-6.jpg
 
-next gallery view imporvement
-https://dribbble.com/shots/4941238-One-Minimal-Festival-Homepage-Alternative-Version-Animation
-
 https://50-jahre-hitparade.ch/ the shit 🔥
 
 https://collagecrafting.com/en
@@ -30,3 +27,16 @@ WORK ON DETAIL
 
 Gallery view travel effect on detail click transition
 https://dribbble.com/shots/7178379-Reproduction-Wellcure
+
+http://peterlindbergh.obys.agency/
+
+https://www.aros.dk/da/laering/skoler-og-institutioner/
+
+scroll left right
+flying effect, scroll with background dots move closer
+
+implement 1 canvas width resp
+
+1.3.20
+
+home https://dribbble.com/shots/8549477-Mobile-Flowchart, https://twitter.com/pbesh/status/1208451307687817217?s=20
--- src/utlis.js
+++ src/utlis.js
@@ -24,46 +24,6 @@ export function createCanvas(width, height) {
   return canvas;
 };
 
-
-// function drawTextAlongArc(context, str, centerX, centerY, radius, angle) {
-//   var len = str.length, s;
-//   context.save();
-//   context.translate(centerX, centerY);
-//   context.rotate(-1 * angle / 2);
-//   context.rotate(-1 * (angle / len) / 2);
-//   for (var n = 0; n < len; n++) {
-//     context.rotate(angle / len);
-//     context.save();
-//     context.translate(0, -1 * radius);
-//     s = str[n];
-//     context.fillText(s, 0, 0);
-//     context.restore();
-//   }
-//   context.restore();
-// }
-
-// function mapsPath(map, name) {
-// 	const features = mesh(map, map.objects[name]);
-
-// 	const projection = geoMercator().fitSize([WIDTH, HEIGHT], features);
-// 	const path = geoPath(projection)
-
-// 	return map.objects[name].geometries.map(d => {
-// 		const p = mesh(map, d);
-// 		return path(p);
-// 	});
-// };
-
-// function mapsPathFeature(map, name) {
-// 	const features = feature(map, map.objects[name]);
-
-// 	const projection = geoMercator().fitSize([WIDTH, HEIGHT], features);
-// 	const path = geoPath().projection(projection);
-
-// 	return path(features);
-// }
-
-
 export default {
   getPixelRatio,
   createCanvas

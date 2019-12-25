40d888c8842fcd88c36f65159fd68e2ec65e47f6
Guruh Hapsara <guruhhapsara@gmail.com>
2019-12-25 21:25:08 +0700

    🌟

--- src/Bahasa.js
+++ src/Bahasa.js
@@ -1,8 +1,8 @@
 import React, { useMemo, useCallback } from 'react';
-import { useLoader, useThree } from 'react-three-fiber';
+import { useThree } from 'react-three-fiber';
 import { a } from 'react-spring/three';
 import { chunk } from 'lodash-es';
-import { mapRange, wrap } from 'canvas-sketch-util/math';
+import { mapRange } from 'canvas-sketch-util/math';
 import { shuffle } from 'canvas-sketch-util/random';
 import * as THREE from 'three';
 import { createCanvas, getPixelRatio } from './utlis';
@@ -16,8 +16,6 @@ import kabkotBahasa from './data/bahasa-kabkot.json';
 //     [key]: curr.bahasa
 //   }
 // }, {});
-
-// console.log(b);
 
 const pixelRatio = getPixelRatio();
 
@@ -34,150 +32,133 @@ const bahasaAll = provinsiBahasa.reduce((prev, curr) => ({
 }), {});
 
 const bahasa = bahasaAll["Jawa Tengah"];
-// const bahasa = bahasaAll["Papua"];
 
-function createTextArray(bahasa, viewport) {
-  let a = []
-  let bl = bahasa.length;
+const FONT_SIZE = 500;
+const FONT = `bold ${FONT_SIZE}px -apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, ubuntu, roboto, noto, segoe ui, arial, sans-serif`;
+
+function createTextArray(bahasa) {
+  const height = FONT_SIZE * 0.5;
+
+  const bhs = shuffle(bahasa);
+  const bhsl = bahasa.length;
+
+  const a = []
   for (let i = 0; i < 50; i++) {
-    const index = (i % bl);
+    const index = (i % bhsl);
 
-    a.push(bahasa[index]);
+    a.push(bhs[index]);
   }
 
-  // const b = chunk(shuffle(a), 10);
   const b = chunk(a, 10);
 
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  context.font = FONT;
+
   const c = b.map(d => {
-    const data = d.reduce((prev, curr, i) => {
-      const width = curr.length * 195;
+    const data = d.reduce((prev, text, i) => {
+      const width = context.measureText(text).width / 2;
 
-      let x0 = 0;
-      if (i > 0) {
-        prev.forEach(e => {
-          x0 += e.width;
-        });
-      }
+      let x = 0;
+      prev.forEach((p) => {
+        x += p.width;
+      });
 
-      const x = ((width / 2) + x0) * 0.003333;
+      x = x + (width / 2);
 
       return [...prev, {
         x,
-        text: curr,
+        text,
         width,
+        height,
       }];
     }, []);
 
     const width = data.reduce((prev, curr) => prev + curr.width, 0);
 
-    const S = viewport.width / 2;
-    const X = width * 0.003333;
-
     return {
       data,
-      S,
-      X
-    }
+      width
+    };
   });
 
   return c;
 }
 
-function Text({ text, x, width }) {
-  const fontSize = 700;
-  const height = 400;
-
-
-  const we = 1;
-  const he = height / width;
-
-  const s = width / 300;
-  const scale = [s, s, s];
-  const position = [x, 0, 0];
-
+function Text({ x, text, width, height }) {
   const canvas = useMemo(() => {
     const canvas = createCanvas(width, height);
     const context = canvas.getContext('2d');
 
-    const w = width * pixelRatio;
     const h = height * pixelRatio;
 
-    context.font = `bold ${fontSize}px -apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, ubuntu, roboto, noto, segoe ui, arial, sans-serif`;
-    // context.textAlign = 'center';
+    const font = `bold ${FONT_SIZE * 0.95}px -apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, ubuntu, roboto, noto, segoe ui, arial, sans-serif`;
+    context.font = font;
+    context.textAlign = 'left';
     context.textBaseline = 'middle';
     context.fillStyle = COLOR;
     context.fillText(text, 0, h * 0.5);
 
-    const a = context.measureText(text).width;
-
-    console.log({ width, a });
-
-    context.beginPath();
-    context.strokeStyle = COLOR;
-    context.lineWidth = 50;
-    context.strokeRect(0, 0, w, h);
-
     return canvas;
-  }, [text, width]);
+  }, [text, width, height]);
+
+  const canvasTexture = new THREE.CanvasTexture(canvas);
+  canvasTexture.minFilter = THREE.LinearFilter;
 
   return (
-    <mesh position={position} scale={scale}>
-      <meshBasicMaterial attach="material" transparent>
-        <canvasTexture attach="map" image={canvas} />
-      </meshBasicMaterial>
-      <planeGeometry attach="geometry" args={[we, he, 1]} />
+    <mesh position={[x, 0, 0]}>
+      <meshBasicMaterial attach="material" transparent map={canvasTexture} />
+      <planeGeometry attach="geometry" args={[width, height, 1]} />
     </mesh>
   );
 }
 
 function Bahasa({ top }) {
-  const { viewport, size } = useThree();
+  const { viewport } = useThree();
 
-  console.log(viewport, size);
-  const texts = createTextArray(bahasa, viewport);
+  const texts = createTextArray(bahasa);
 
+  const x0 = viewport.width / 2;
+  const s = 0.005;
   const r = [-3, -1.5, 0, 1.5, 3];
 
-  const toRight = useCallback((s0, s1) => {
-    return (t) => mapRange(t, 0, SCROLL_VIEW_HEIGHT, s0, -s1 - s0)
-  }, []);
+  const toLeft = useCallback((width) => {
+    return (t) => {
+      return mapRange(t, 0, SCROLL_VIEW_HEIGHT, x0, (-width * s) - x0);
+    }
+  }, [x0]);
 
-  const toLeft = useCallback((s0, s1) => {
-    return (t) => mapRange(t, 0, SCROLL_VIEW_HEIGHT, -s1 - s0, s0);
-  }, []);
-
-  const x = (viewport.width / 2) * -1;
+  const toRight = useCallback((width) => {
+    return (t) => {
+      return mapRange(t, 0, SCROLL_VIEW_HEIGHT, (-width * s) - x0, x0);
+    }
+  }, [x0]);
 
   return (
-    <group scale={[0.5, 0.5, 0.5]} position={[x, 0, 0]}>
-      {texts[0].data.map((t, i) => (
-        <Text key={i} {...t} />
-      ))}
-    </group>
+    <>
+      {texts.map((txt, i) => {
+        const { data, width } = txt;
+        const interp = i === 1 || i === 3 ? toLeft(width) : toRight(width);
+        const y = r[i];
+
+        return (
+          <a.group
+            key={i}
+            position={top.interpolate(t => {
+              const x = interp(t);
+
+              return [x, y, 0];
+            })}
+            scale={[s, s, s]}
+          >
+            {data.map((t, j) =>
+              <Text key={i + j} {...t} />
+            )}
+          </a.group>
+        )
+      })}
+    </>
   );
-
-  // return (
-  //   <>
-  //     {texts.map((txt, i) => {
-  //       const interp = i === 1 || i === 3 ? toLeft(txt.S, txt.X) : toRight(txt.S, txt.X);
-  //       const y = r[i];
-  //       return (
-  //         <a.group
-  //           key={i}
-  //           position={top.interpolate(t => {
-  //             const x = interp(t);
-
-  //             return [x, y, 0];
-  //           })}
-  //         >
-  //           {txt.data.map((t, j) =>
-  //             <Text key={i + j} {...t} />
-  //           )}
-  //         </a.group>
-  //       )
-  //     })}
-  //   </>
-  // );
 }
 
 

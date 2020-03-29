4677a25ed1e4ac843204640f34dea7d705ce4369
Guruh Hapsara <guruhhapsara@gmail.com>
2020-03-29 21:47:06 +0700

    🗾

---
+++ src/maps/index.js
@@ -0,0 +1,158 @@
+import indonesia from './indonesia.json';
+import aceh from './aceh.json';
+import bali from './bali.json';
+import banten from './banten.json';
+import bengkulu from './bengkulu.json';
+import gorontalo from './gorontalo.json';
+import jakarta from './jakarta.json';
+import jambi from './jambi.json';
+import jawaBarat from './jawa-barat.json';
+import jawaTengah from './jawa-tengah.json';
+import jawaTimur from './jawa-timur.json';
+import kalimantanBarat from './kalimantan-barat.json';
+import kalimantanSelatan from './kalimantan-selatan.json';
+import kalimantanTengah from './kalimantan-tengah.json';
+import kalimantanTimur from './kalimantan-timur.json';
+import kalimantanUtara from './kalimantan-utara.json';
+import kepulauanBangkaBelitung from './kepulauan-bangka-belitung.json';
+import kepulauanRiau from './kepulauan-riau.json';
+import kepulauanSeribu from './kepulauan-seribu.json';
+import lampung from './lampung.json';
+import malukuUtara from './maluku-utara.json';
+import maluku from './maluku.json';
+import nusaTenggaraBarat from './nusa-tenggara-barat.json';
+import nusaTenggaraTimur from './nusa-tenggara-timur.json';
+import papuaBarat from './papua-barat.json';
+import papua from './papua.json';
+import riau from './riau.json';
+import sulawesiBarat from './sulawesi-barat.json';
+import sulawesiSelatan from './sulawesi-selatan.json';
+import sulawesiTengah from './sulawesi-tengah.json';
+import sulawesiTenggara from './sulawesi-tenggara.json';
+import sulawesiUtara from './sulawesi-utara.json';
+import sumateraBarat from './sumatera-barat.json';
+import sumateraSelatan from './sumatera-selatan.json';
+import sumateraUtara from './sumatera-utara.json';
+import yogyakarta from './yogyakarta.json';
+
+// export 
+
+// export const maps = [
+//   aceh,
+//   bali,
+//   banten,
+//   bengkulu,
+//   gorontalo,
+//   indonesia,
+//   jakarta,
+//   jambi,
+//   jawaBarat,
+//   jawaTengah,
+//   jawaTimur,
+//   kalimantanBarat,
+//   kalimantanSelatan,
+//   kalimantanTengah,
+//   kalimantanTimur,
+//   kalimantanUtara,
+//   kepulauanBangkaBelitung,
+//   kepulauanRiau,
+//   kepulauanSeribu,
+//   lampung,
+//   malukuUtara,
+//   maluku,
+//   nusaTenggaraBarat,
+//   nusaTenggaraTimur,
+//   papuaBarat,
+//   papua,
+//   riau,
+//   sulawesiBarat,
+//   sulawesiSelatan,
+//   sulawesiTengah,
+//   sulawesiTenggara,
+//   sulawesiUtara,
+//   sumateraBarat,
+//   sumateraSelatan,
+//   sumateraUtara,
+//   yogyakarta,
+// ];
+
+export const maps = {
+  'aceh': aceh,
+  'bali': bali,
+  'banten': banten,
+  'bengkulu': bengkulu,
+  'gorontalo': gorontalo,
+  // 'indonesia': indonesia,
+  'jakarta': jakarta,
+  'jambi': jambi,
+  'jawa-barat': jawaBarat,
+  'jawa-tengah': jawaTengah,
+  'jawa-timur': jawaTimur,
+  'kalimantan-barat': kalimantanBarat,
+  'kalimantan-selatan': kalimantanSelatan,
+  'kalimantan-tengah': kalimantanTengah,
+  'kalimantan-timur': kalimantanTimur,
+  'kalimantan-utara': kalimantanUtara,
+  'kepulauan-bangka-belitung': kepulauanBangkaBelitung,
+  'kepulauan-riau': kepulauanRiau,
+  'kepulauan-seribu': kepulauanSeribu,
+  'lampung': lampung,
+  'maluku-utara': malukuUtara,
+  'maluku': maluku,
+  'nusa-tenggara-barat': nusaTenggaraBarat,
+  'nusa-tenggara-timur': nusaTenggaraTimur,
+  'papua-barat': papuaBarat,
+  'papua': papua,
+  'riau': riau,
+  'sulawesi-barat': sulawesiBarat,
+  'sulawesi-selatan': sulawesiSelatan,
+  'sulawesi-tengah': sulawesiTengah,
+  'sulawesi-tenggara': sulawesiTenggara,
+  'sulawesi-utara': sulawesiUtara,
+  'sumatera-barat': sumateraBarat,
+  'sumatera-selatan': sumateraSelatan,
+  'sumatera-utara': sumateraUtara,
+  'yogyakarta': yogyakarta,
+}
+
+// export const maps = {
+//   'Aceh': aceh,
+//   'Bali': bali,
+//   'Banten': banten,
+//   'Bengkulu': bengkulu,
+//   'Gorontalo': gorontalo,
+//   'Jakarta': jakarta,
+//   'Indonesia': ,
+//   'Jambi': jambi,
+//   'Jawa Barat': jawaBarat,
+//   'Jawa Tengah': jawaTengah,
+//   'Jawa Timur': jawaTimur,
+//   'Kalimantan Barat': kalimantanBarat,
+//   'Kalimantan Selatan': kalimantanSelatan,
+//   'Kalimantan Tengah': kalimantanTengah,
+//   'Kalimantan Timur': kalimantanTimur,
+//   'Kalimantan Utara': kalimantanUtara,
+//   'Kepulauan Bangka Belitung': kepulauanBangkaBelitung,
+//   'Kepulauan Riau': kepulauanRiau,
+//   'Kepulauan Seribu': kepulauanSeribu,
+//   'Lampung': lampung,
+//   'Maluku Utara': malukuUtara,
+//   'Maluku': maluku,
+//   'Nusa Tenggara Barat': nusaTenggaraBarat,
+//   'Nusa Tenggara Timur': nusaTenggaraTimur,
+//   'Papua Barat': papuaBarat,
+//   'Papua': papua,
+//   'Riau': riau,
+//   'Sulawesi Barat': sulawesiBarat,
+//   'Sulawesi Selatan': sulawesiSelatan,
+//   'Sulawesi Tengah': sulawesiTengah,
+//   'Sulawesi Tenggara': sulawesiTenggara,
+//   'Sulawesi Utara': sulawesiUtara,
+//   'Sumatera Barat': sumateraBarat,
+//   'Sumatera Selatan': sumateraSelatan,
+//   'Sumatera Utara': sumateraUtara,
+//   'Yogyakarta': yogyakarta,
+// }
+
+export default maps;
+
